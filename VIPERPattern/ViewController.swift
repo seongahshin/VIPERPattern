3933d9528cee1177e40f824d1c636cd5f317c155
seongahshin <cindy903c@naver.com>
2024-07-06 17:30:10 +0900

    Initial Commit

---
+++ VIPERPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPERPattern
+//
+//  Created by 신승아 on 7/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
